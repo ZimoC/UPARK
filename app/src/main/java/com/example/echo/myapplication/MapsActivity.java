acff1b19c62b1d544b692c479cc5e22c1a68835c
Zimo Chen <echozmchen@gmail.com>
2017-05-02 20:11:34 -0400

    Chore: write application’s basic functions

---
+++ app/src/main/java/com/example/echo/myapplication/MapsActivity.java
@@ -0,0 +1,215 @@
+package com.example.echo.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Geocoder;
+import android.net.Uri;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.identity.intents.Address;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private static final LatLng p = new LatLng(42.3889167, -71.2208033);
+    private static final LatLng p1 = new LatLng(42.384802, -71.218219);
+    private static final LatLng p2 = new LatLng(42.386396, -71.225954);
+    private static final LatLng p3 = new LatLng(42.366146, -71.228616);
+
+    private static final String addr = "175 Forest Street";
+    private static final String addr1 = "1-99 Cedar Hill Ln, Waltham";
+    private static final String addr2 = "16 Forest Street";
+    private static final String addr3 = "196 High St, Waltham";
+
+    String addList[] = {"1-99 Cedar Hill Ln, Waltham", "16 Forest Street", "196 High St, Waltham"};
+
+
+    private static final float zoom = 14.0f;
+    private TabHost tabs;
+    private Button button;
+    private TabHost.TabSpec spec;
+    private TextView tvText;
+    private EditText etText;
+    private TextToSpeech tts;
+    private ListView listview;
+    private ArrayAdapter adapter;
+    private ArrayList<String> list = new ArrayList<>();
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        tabs = (TabHost) findViewById(R.id.tabhost);
+        tabs.setup();
+
+        // Initialize a TabSpec for tab1 and add it to the TabHost
+        spec = tabs.newTabSpec("tag1");    //create new tab specification
+        spec.setContent(R.id.tab1);    //add tab view content
+        spec.setIndicator("Map");    //put text on tab
+        tabs.addTab(spec);             //put tab in TabHost container
+
+        button = (Button) findViewById(R.id.searchButton);
+        etText = (EditText) findViewById(R.id.searchET);
+
+  /* Initialize a TabSpec for tab2 and add it to the TabHost */
+        spec = tabs.newTabSpec("tag2");        //create new tab specification
+        spec.setContent(R.id.tab2);            //add view tab content
+        spec.setIndicator("List");              //put text on tab
+        tabs.addTab(spec);                     //put tab in TabHost container
+
+   /* Initialize a TabSpec for tab3 and add it to the TabHost */
+        spec = tabs.newTabSpec("tag3");        //create new tab specification
+        spec.setContent(R.id.tab3);            //add view tab content
+        spec.setIndicator("Share");              //put text on tab
+        tabs.addTab(spec);                     //put tab in TabHost container
+
+
+        //----------------------
+        // Get ListView from XML
+        listview = (ListView) findViewById(R.id.list);
+
+        for (int i = 0; i < addList.length; ++i) {
+            list.add(addList[i]);
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                String item = (String) parent.getItemAtPosition(position);
+            }
+        });
+    }
+
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        button = (Button) findViewById(R.id.searchButton);
+
+        mMap.addMarker(new MarkerOptions().position(p).title("Bentley University").snippet(addr));
+
+        mMap.addMarker(new MarkerOptions().position(p1).snippet(addr1));
+
+        mMap.addMarker(new MarkerOptions().position(p2).snippet(addr2));
+
+        mMap.addMarker(new MarkerOptions().position(p3).snippet(addr3));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(p3, zoom));
+
+
+        mMap.setOnMarkerClickListener(
+                new GoogleMap.OnMarkerClickListener() {
+
+                    public boolean onMarkerClick(Marker m) {
+                        String title = m.getSnippet();
+                        String snip = m.getSnippet();
+                        Toast.makeText(getApplicationContext(), title + "\n" + snip, Toast.LENGTH_LONG).show();
+
+                        if (snip.equals(addr)) {
+                            tabs.setCurrentTabByTag("tag2");
+                        }
+                        if (snip.equals(addr1)) {
+                            tabs.setCurrentTabByTag("tag2");
+                        }
+                        if (snip.equals(addr2)) {
+                            tabs.setCurrentTabByTag("tag2");
+                        }
+                        if (snip.equals(addr3)) {
+                            tabs.setCurrentTabByTag("tag2");
+                        }
+                        return true;
+                    }
+                }
+        );
+
+        mMap.setOnMapLongClickListener(
+                new GoogleMap.OnMapLongClickListener() {
+                    public void onMapLongClick(LatLng point) {
+                        Toast.makeText(getApplicationContext(), "Long Tap", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+
+    //-------------
+
+    // Implements TextToSpeech.OnInitListener
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = tts.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("Error", "This Language is not supported");
+            } else {
+                addToSpeech("Please enter your to do list");
+            }
+        } else
+            Log.e("Error", "Failed");
+    }
+
+    public void addToSpeech(String str) {
+        tts.speak(str, TextToSpeech.QUEUE_FLUSH, null);
+    }
+}
+ /*   protected void onPause() {
+        // shut down TextToSpeech
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
+    } */
+
+
+
+
+
+
+
