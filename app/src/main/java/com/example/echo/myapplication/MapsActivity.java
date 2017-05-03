2730642d74ac212263ac2fbd6fa09b6dfd8db920
Danna Dai <dai_dann@bentley.edu>
2017-05-03 17:43:21 -0400

    Fixes

--- app/src/main/java/com/example/echo/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/echo/myapplication/MapsActivity.java
@@ -263,14 +263,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         EditText location_tf = (EditText) findViewById(R.id.searchET);
         String location = location_tf.getText().toString();
-<<<<<<< HEAD
-        //set text to speech content
-      //  String tospeech=("Searching parking spaces around"+location);
-        tts.speak(location, TextToSpeech.QUEUE_FLUSH, null);
-=======
-        String tospeach= ("searching parking spaces around"+location);
-        tts.speak(tospeach, TextToSpeech.QUEUE_FLUSH, null);
->>>>>>> origin/master
         List<android.location.Address> addressList = null;
         if (location != null || !location.equals("")) {
             Geocoder geocoder = new Geocoder(this);
